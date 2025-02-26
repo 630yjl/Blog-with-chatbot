2b9c849a0bdf6ffd674c3208b446b3777d197ac6
yunjeongLim <630yjl@gmail.com>
2025-02-26 23:20:11 +0900

    TailwindCSS 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,51 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+  ...compat.extends(
+    'next/core-web-vitals',
+    'next/typescript',
+    'prettier',
+    'plugin:tailwindcss/recommended',
+    'eslint-disable @typescript-eslint/no-unused-vars',
+  ),
+  {
+    rules: {
+      'tailwindcss/classnames-order': [
+        'warn',
+        {
+          callees: ['cva', 'cn'],
+        },
+      ],
+
+      'tailwindcss/enforces-negative-arbitrary-values': [
+        'warn',
+        {
+          callees: ['cva', 'cn'],
+        },
+      ],
+
+      'tailwindcss/enforces-shorthand': [
+        'warn',
+        {
+          callees: ['cva', 'cn'],
+        },
+      ],
+
+      'tailwindcss/no-contradicting-classname': [
+        'warn',
+        {
+          callees: ['cva', 'cn'],
+        },
+      ],
+
+      'tailwindcss/no-custom-classname': [
+        'warn',
+        {
+          callees: ['cva', 'cn'],
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
