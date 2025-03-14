0f82cfdce31ab4b61c701014badbd5fa83d7059f
yunjeongLim <630yjl@gmail.com>
2025-03-14 19:30:52 +0900

    무한 스크롤 구현(useInfiniteQuery,intersection-observer)

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -19,7 +19,7 @@ const PostCard: FC<PostCardProps> = ({
     <Link href={`/posts/${id}`} className={cn('bg-white', className)}>
       <div className="relative aspect-[1.8/1] w-full">
         <Image
-          src={preview_image_url ?? '/thumnaul.svg'}
+          src={preview_image_url ?? '/thumbnail.svg'}
           fill
           sizes="360px"
           alt={title}
